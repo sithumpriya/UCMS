ad4c471098e298030876fec1231fc88d2ceb3378
CT-2019-087 Sakuntha <sakunthaprabodha@gmail.com>
2025-09-01 19:16:44 +0530

    Added course dashboard page

---
+++ BulkyWeb/Areas/Admin/Controllers/CourseDashboardController.cs
@@ -0,0 +1,88 @@
+﻿using WMS.DataAccess.Data;
+using WMS.DataAccess.Repository.IRepository;
+using WMS.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Bulky.Utility;
+
+namespace WMSWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = SD.Role_Employee)]
+    public class CourseDashboardController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public CourseDashboardController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            var courses = _unitOfWork.Course.GetAll();
+            ViewBag.Courses = courses;
+
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var lectureNotes = _unitOfWork.LectureNote.GetAll(c => c.CourseId == id);
+            ViewBag.LectureNotes = lectureNotes;
+
+            ViewBag.Course = _unitOfWork.Course.Get(c => c.Id == id);
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Course obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Course.Update(obj);
+                _unitOfWork.Save();
+                TempData["success"] = "Course updated successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        #region API CALLS
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<Course> objCourseList = _unitOfWork.Course.GetAll().ToList();
+            return Json(new { data = objCourseList });
+        }
+
+
+        [HttpDelete]
+        public IActionResult Delete(int? id)
+        {
+            var courseToBeDeleted = _unitOfWork.Course.Get(u => u.Id == id);
+            if (courseToBeDeleted == null)
+            {
+                return Json(new { success = false, message = "Error while deleting" });
+            }
+
+            _unitOfWork.Course.Remove(courseToBeDeleted);
+            _unitOfWork.Save();
+
+            return Json(new { success = true, message = "Delete Successful" });
+        }
+
+        #endregion
+    }
+}
