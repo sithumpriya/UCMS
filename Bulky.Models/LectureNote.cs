860fc35c15996b994feeb1f4b33406f0cfd4233f
CT-2019-087 Sakuntha <sakunthaprabodha@gmail.com>
2025-09-01 15:08:36 +0530

    Added LectureNote page

---
+++ Bulky.Models/LectureNote.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace WMS.Models
+{
+    public class LectureNote
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [DisplayName("Course")]
+        [ValidateNever]
+        public int? CourseId { get; set; }
+
+        [ForeignKey("CourseId")]
+        [ValidateNever]
+        public Course Course { get; set; }
+
+        [Required]
+        public string Note { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        public string Title { get; set; }
+
+        [DisplayName("Display Order")]
+        [Range(1, 100, ErrorMessage = "Display Order must be between 1-100")]
+        public int DisplayOrder { get; set; }
+    }
+}
---
+++ BulkyWeb/Areas/Admin/Controllers/LectureNoteController.cs
@@ -0,0 +1,113 @@
+﻿using WMS.DataAccess.Data;
+using WMS.DataAccess.Repository.IRepository;
+using WMS.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Bulky.Utility;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace WMSWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = SD.Role_Employee)]
+    public class LectureNoteController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public LectureNoteController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            IEnumerable<SelectListItem> CourseList = _unitOfWork.Course.GetAll().Select(u => new SelectListItem
+            {
+                Text = u.Name,
+                Value = u.Id.ToString()
+            });
+            ViewBag.CourseList = CourseList;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(LectureNote obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.LectureNote.Add(obj);
+                _unitOfWork.Save();
+                TempData["success"] = "LectureNote created successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            IEnumerable<SelectListItem> CourseList = _unitOfWork.Course.GetAll().Select(u => new SelectListItem
+            {
+                Text = u.Name,
+                Value = u.Id.ToString()
+            });
+            ViewBag.CourseList = CourseList;
+
+            LectureNote? lectureNoteFromDb1 = _unitOfWork.LectureNote.Get(c => c.Id == id);
+
+            if (lectureNoteFromDb1 == null)
+            {
+                return NotFound();
+            }
+            return View(lectureNoteFromDb1);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(LectureNote obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.LectureNote.Update(obj);
+                _unitOfWork.Save();
+                TempData["success"] = "LectureNote updated successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        #region API CALLS
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<LectureNote> objLectureNoteList = _unitOfWork.LectureNote.GetAll(includeProperties: "Course").ToList();
+            return Json(new { data = objLectureNoteList });
+        }
+
+
+        [HttpDelete]
+        public IActionResult Delete(int? id)
+        {
+            var lectureNoteToBeDeleted = _unitOfWork.LectureNote.Get(u => u.Id == id);
+            if (lectureNoteToBeDeleted == null)
+            {
+                return Json(new { success = false, message = "Error while deleting" });
+            }
+
+            _unitOfWork.LectureNote.Remove(lectureNoteToBeDeleted);
+            _unitOfWork.Save();
+
+            return Json(new { success = true, message = "Delete Successful" });
+        }
+
+        #endregion
+    }
+}
